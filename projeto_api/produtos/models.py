cc8c321027993119fa51a084c065ba8b835a319c
s2ddv <1speedftn@gmail.com>
2025-04-10 13:39:31 -0300

    docs: organize files

---
+++ projeto_api/produtos/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+class Categoria(models.Model):
+    nome = models.CharField(max_length = 100)
+
+    def __str__(self):
+        return self.nome 
+
+class Produto(models.Model):
+    nome = models.CharField(max_length = 100)
+    descricao = models.TextField()
+    preco = models.DecimalField(max_digits = 10, decimal_places = 2)
+    categoria = models.ForeignKey(Categoria, related_name='produtos', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.nome 
