9afbff6a7d1512ccd4a16052e1ea55ba4a2cc081
s2ddv <1speedftn@gmail.com>
2025-04-17 10:49:46 -0300

    docs - organize files

--- projeto_api/produtos/models.py
+++ projeto_api/produtos/models.py
@@ -11,6 +11,7 @@ class Produto(models.Model):
     descricao = models.TextField()
     preco = models.DecimalField(max_digits = 10, decimal_places = 2)
     categoria = models.ForeignKey(Categoria, related_name='produtos', on_delete=models.CASCADE)
-
+    imagem = models.ImageField(upload_to='produtos/', blank=True, null=True)
+    
     def __str__(self):
         return self.nome 
